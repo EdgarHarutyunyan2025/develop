79fd7bc9812dca34cc60e4b617e01a26c3d4bd15
edgar_harutyunyan_2025 <edgar_harutyunyan_2025@mail.ru>
2025-06-24 16:05:55 +0400

    v7

--- jsonplaceholder/index.mjs
+++ jsonplaceholder/index.mjs
@@ -5,7 +5,7 @@ const app = express()
 const PORT = 3000
 
 app.get("/", (req, res) => {
-  const helloMessage = `<h1>VERSION 6: Hello from the ArgoCD ---> ${os.hostname()}</h1>`
+  const helloMessage = `<h1>VERSION 7: Hello from the ArgoCD ---> ${os.hostname()}</h1>`
   console.log(helloMessage)
   res.send(helloMessage)
 })
